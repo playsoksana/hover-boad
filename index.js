53d4ea2840f3f3a822750932d37269f7ee1214fa
playsoksana <playsoksana@gmail.com>
2021-10-03 22:37:04 +0300

    cleared  code

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ const randomColor = () => {
 for (let i = 0; i < SQ_NUM; i++) {
   const qs = document.createElement("div");
   qs.classList.add("square");
-  qs.addEventListener("mouseover", (e) => {
-    setColor(qs);
-  });
-  qs.addEventListener("mouseleave", (e) => {
-    removeColor(qs);
-  });
+  qs.addEventListener("mouseover", setColor);
+  qs.addEventListener("mouseleave", removeColor);
   board.append(qs);
 }
 
 function setColor(el) {
-  el.style.backgroundColor = `${randomColor()}`;
-  el.style.boxShadow = `0 0 2px ${randomColor()}, 0 0 10px ${randomColor()}`;
+  el.target.style.backgroundColor = `${randomColor()}`;
+  el.target.style.boxShadow = `0 0 2px ${randomColor()}, 0 0 10px ${randomColor()}`;
 }
 function removeColor(el) {
-  el.style.backgroundColor = "#1d1d1d";
-  el.style.boxShadow = "0 0 2px #1d1d1d";
+  el.target.style.backgroundColor = "#1d1d1d";
+  el.target.style.boxShadow = "0 0 2px #1d1d1d";
 }
